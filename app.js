40ec49d6839dc3bf937c63e602d877e813f1883c
Aditya Chandel <aditya.chandel@aggiemail.usu.edu>
2017-06-25 23:03:40 -0600

    50% Sale

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
 app.get('/', function (req, res) {
-  res.json({"foo": "bar"});
-})
+    res.json({"foo": "bar"});
+});
 
 app.get('/dashbanner', function (req, res) {
-  res.json({"dash": "banner"});
-})
+    res.json({
+        "title": "Sale: 50% Off (Limited Time)",
+        "message": "For limited time get the Quiz Time: Pro for 50% off regular price, i.e. only $0.5. Quiz Time: Pro features: \n" +
+        "- 30000 more unique questions. (Free version has 7500 only Questions) \n" +
+        "- 4 more game modes (Multi Topics, Millionaire, Non-Stop, 3-Difficulty +\n" +
+        "- Questions from 11 more exciting Topic! +\n" +
+        "- No Advertisements \n" +
+        "- Support a small developer pay his bills. :)",
+        "pos": "Get Pro",
+        "show": "True",
+        "version": "1"
+    });
+});
 
 app.listen((process.env.PORT || 3000), function () {
-  console.log('Example app listening on port 3000!')
-})
+    console.log('Example app listening on port 3000!')
+});
