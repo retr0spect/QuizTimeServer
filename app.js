63e00ee2a352a56e0909cdc0f1294c45f6a7f5c3
Aditya Chandel <aditya.chandel@aggiemail.usu.edu>
2017-06-25 23:05:16 -0600

    50% Sale

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ app.get('/', function (req, res) {
 app.get('/dashbanner', function (req, res) {
     res.json({
         "title": "Sale: 50% Off (Limited Time)",
-        "message": "For limited time get the Quiz Time: Pro for 50% off regular price, i.e. only $0.5. Quiz Time: Pro features: \n" +
-        "- 30000 more unique questions. (Free version has 7500 only Questions) \n" +
-        "- 4 more game modes (Multi Topics, Millionaire, Non-Stop, 3-Difficulty +\n" +
-        "- Questions from 11 more exciting Topic! +\n" +
-        "- No Advertisements \n" +
-        "- Support a small developer pay his bills. :)",
+        "message": "For limited time get the Quiz Time: Pro for 50% off regular price, i.e. only $0.5. Quiz Time: Pro features: \n\n" +
+        "1. 30000 more unique questions. (Free version has 7500 only Questions) \n" +
+        "2. 4 more game modes (Multi Topics, Millionaire, Non-Stop, 3-Difficulty \n" +
+        "3. Questions from 11 more exciting Topic! \n" +
+        "4. No Advertisements \n" +
+        "5. Support a small developer pay his bills. :)",
         "pos": "Get Pro",
         "show": "True",
         "version": "1"
